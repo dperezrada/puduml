46fddbd8025ec69bf47bfee42d3eb5a2e5dbe104
Daniel Pérez Rada <dperezrada@gmail.com>
2017-07-29 16:15:52 -0400

    ADD: Multicore for prepare file (slowest part of the process)

--- classification_model/predict.sh
+++ classification_model/predict.sh
@@ -4,15 +4,18 @@ INPUT_PATH=$1
 MODEL_PATH=$2
 FEATURES_PATH=$3
 PREPARED_FILE=$4
+N_PROCESS=$5
 
-N_PROCESS=3
+if [[ ! -z "$N_PROCESS" ]]; then
+	N_PROCESS=2
+fi
 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 
-python ${SCRIPT_DIR}/prepare_file.py ${INPUT_PATH} ${FEATURES_PATH} ${PREPARED_FILE}
+python ${SCRIPT_DIR}/prepare_file.py ${INPUT_PATH} ${FEATURES_PATH} ${PREPARED_FILE} ${N_PROCESS}
 
 python ${SCRIPT_DIR}/predict.py ${PREPARED_FILE} ${MODEL_PATH}
 
